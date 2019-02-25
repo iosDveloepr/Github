81b280bfd6daed68b0cb27db8f84abdf77030238
Yermakov Anton <yermakovanton@MacBook-Pro-Yermakov.local>
2019-02-25 12:49:42 +0200

    Initial Commit

---
+++ Github Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github Search
+//
+//  Created by Yermakov Anton on 2/25/19.
+//  Copyright © 2019 Yermakov Anton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
